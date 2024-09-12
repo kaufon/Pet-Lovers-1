995f49e0db768947504ba7609def9cb2a034ee0a
kaufon <kauanfvale@gmail.com>
2024-09-12 17:37:27 -0300

    👥entities: add abstract entities

---
+++ core/domain/entities/Company.ts
@@ -0,0 +1,28 @@
+import { Client } from "./Client";
+import { Product } from "./Product";
+import { Service } from "./Service";
+
+type CompanyProps = {
+  _clients: Client[];
+  _products: Product[];
+  _services: Service[];
+};
+export class Company {
+  private _clients: Client[];
+  private _products: Product[];
+  private _services: Service[];
+  constructor(props:CompanyProps){
+    props._services = this._services
+    props._clients = this._clients
+    props._products = this._products
+  }
+  public get getClients(): Client[]{
+    return this._clients
+  }
+  public get getServices(): Service[]{
+    return this._services
+  }
+  public get getProducts(): Product[]{
+    return this._products
+  }
+}
--- core/domain/entities/index.ts
+++ core/domain/entities/index.ts
@@ -1,1 +1,8 @@
-export {} from "./Client"
+export { Client } from "./Client";
+export { Company } from "./Company";
+export { Cellphone } from "./Cellphone";
+export { Rg } from "./Rg";
+export { Cpf } from "./Cpf";
+export { Product } from "./Product";
+export { Service } from "./Service";
+export { Pets } from "./Pet";
