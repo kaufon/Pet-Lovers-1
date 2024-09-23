cba8bcc26a3ffa70dfb28d5756e53f188d86e6c8
kaufon <kauanfvale@gmail.com>
2024-09-23 18:18:20 -0300

    ♻️refactor: refctor entity id

--- core/domain/abstracts/Entity.ts
+++ core/domain/abstracts/Entity.ts
@@ -1,7 +1,10 @@
+
+
+let currentID = 0
 export class Entity{
   protected _id: string
   constructor( _id?:string){
-    this._id = _id ?? crypto.randomUUID()
+    this._id = ( _id !== undefined ? _id: currentID++).toString()
   }
   public get getID(): string{
     return this._id
