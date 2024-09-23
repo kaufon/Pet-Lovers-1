076e7dcb9db79b37a5eb06a41021c3409a7cd240
kaufon <kauanfvale@gmail.com>
2024-09-23 16:28:01 -0300

    ♻️refactor: petloverssystem

--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,70 @@
-import { Input } from "../core/domain/abstracts/Input";
-import { Client } from "../core/domain/entities/Client";
-import { Company } from "../core/domain/entities/Company";
-import { RegisterClientUseCase } from "../core/domain/use-cases/client";
+import { RegisterClientUseCase } from "../core/domain/use-cases/client/register-client";
+import { Input } from "./Input";
+import { Company } from "../core/domain/entities";
 import { LineBreaker } from "./lineBreaker";
-
-console.log("Ola bem vindo ao Pet Lovers!");
-let isRunning = true;
-while (isRunning) {
-  console.log("Opções: ");
-  console.log(`1 - Cadastrar cliente`);
-  console.log(`2 - Listar todos os clientes`);
-  console.log(`0 - Sair`);
-
-  let input = new Input();
-  let option = input.numberInput("Por favor selecione: ");
-  switch (option) {
-    case 1:
-      const useCase = new RegisterClientUseCase(Client["ola"]);
-      useCase.register();
-      break;
-    case 2:
-      console.log("listagem nao feito :(");
-      break;
-    case 0:
-      console.log("Obrigado por usar!");
-
-      isRunning = false;
-      break;
-    default:
-      console.log("Por favor repita não entendi :(");
-      break;
+import { ListClientUseCase } from "../core/domain/use-cases/list/list-client";
+export class PetLoversSystem {
+  private company: Company;
+  private input: Input;
+  constructor() {
+    this.company = new Company();
+    this.input = new Input();
   }
-  LineBreaker();
+  public async run(): Promise<void> {
+    console.log("Ola bem vindo ao Pet Lovers!");
+    let isRunning = true;
+    while (isRunning) {
+      let option = await this.input.selectInput("Por favor selecione :)", [
+        ["Clientes", "clients"],
+        ["Pets", "pets"],
+        ["Sair", "leave"],
+      ]);
+      switch (option) {
+        case "clients":
+          await this.clientHandler();
+          break;
+
+        case "pets":
+          console.log("Oops nao exite :(");
+          break;
+        case "leave":
+          console.log("Obrigado por usar!");
+          isRunning = false;
+          break;
+        default:
+          console.log("Por favor repita não entendi :(");
+          break;
+      }
+      LineBreaker();
+    }
+  }
+  private async clientHandler(): Promise<void> {
+    let option = await this.input.selectInput("Por favor selecione :)", [
+      ["Cadastrar clientes", "register"],
+      ["Listar clientes", "list"],
+      ["Editar clientes", "edit"],
+      ["Deletar clientes", "delete"],
+      ["Voltar", "back"],
+    ]);
+    switch (option) {
+      case "register": {
+        const useCase = new RegisterClientUseCase(
+          this.company.getClients,
+          this.input,
+        );
+        return useCase.register();
+      }
+
+      case "list": {
+        const useCase = new ListClientUseCase(this.company.getClients);
+        return useCase.list();
+      }
+      case "back":
+        return;
+      default:
+        console.log("Nao entendi :(");
+    }
+  }
 }
+const app = new PetLoversSystem()
+app.run()
