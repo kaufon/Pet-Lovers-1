4e24969f6e08e2cff4b9c329b3dd5c838764d036
kaufon <kauanfvale@gmail.com>
2024-09-24 10:28:53 -0300

    💡feat: delete pet

---
+++ core/use-cases/pets/delete-pet.ts
@@ -0,0 +1,39 @@
+import { Client } from "../../domain/entities"
+import { IInput } from "../../interfaces"
+
+export class deletePetUseCase {
+  _clients: Client[]
+  input: IInput
+  constructor(client: Client[], input: IInput) {
+    this._clients= client
+    this.input = input
+  }
+  public async execute(): Promise<void> {
+    console.log("Deletando pet....")
+    const clientID = await this.input.textInput(
+      "Por favor insira o id do client"
+    )
+    const client = this._clients.find((client) => client.getID === clientID)
+    if (!client) {
+      console.log("Cliente nao encontrado")
+      return
+      
+    }
+    const pets = client.getPets
+    if (pets.length === 0) {
+      console.log("O cliente não possui pets")
+      return
+      
+    }
+    const petID = await this.input.textInput("Por favor insira o ID do pet a ser removido")
+    const petToBeRemoved = pets.find((pet) => pet.getID === petID)
+    if (!petToBeRemoved) {
+      console.log("Pet não encontrado")
+      return
+      
+    }
+    const index = pets.indexOf(petToBeRemoved)
+    pets.splice(index, 1)
+    console.log("Pet removido com sucesso")
+  }
+}
