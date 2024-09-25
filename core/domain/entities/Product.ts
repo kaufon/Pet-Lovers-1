410559f5b592ead7075a0c05a6d76e931496c4a5
kaufon <kauanfvale@gmail.com>
2024-09-25 13:53:48 -0300

    ♻️refactor: refactor product class

--- core/domain/entities/Product.ts
+++ core/domain/entities/Product.ts
@@ -1,6 +1,15 @@
+import { Entity } from "../abstracts"
+
 type ProductProps = {
+  price: number
   name:string
 }
-export class Product {
+export class Product extends Entity{
+  public price: number
   public name: string
+  constructor(props:ProductProps){
+    super()
+    this.name = props.name
+    this.price = props.price
+  }
 }
