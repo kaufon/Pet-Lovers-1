d97cd3cffd00b02ffa6e8519eefcd93c059384ce
kaufon <kauanfvale@gmail.com>
2024-09-25 16:07:31 -0300

    💡feat: able to edit rg and phones

--- core/use-cases/client/edit-client.ts
+++ core/use-cases/client/edit-client.ts
@@ -1,4 +1,4 @@
-import { Client, Cpf } from "../../domain/entities";
+import { Cellphone, Client, Cpf, Rg } from "../../domain/entities";
 import { IInput } from "../../interfaces/Input";
 
 export class editClientUseCase {
@@ -12,14 +12,13 @@ export class editClientUseCase {
     const idToBeEdited = await this.input.textInput(
       "Qual id do cliente que você quer editar?",
     );
-    const clientIndex = this._clients.findIndex(
+    const clientToBeEdited = this._clients.find(
       (client) => client.getID === idToBeEdited,
     );
-    if (clientIndex === -1) {
+    if (!clientToBeEdited) {
       console.log("Esse cliente não existe");
       return;
     }
-    const clientToBeEdited: Client = this._clients[clientIndex];
     const newName =
       (await this.input.textInput("Por favor insira o novo nome")) ||
       clientToBeEdited.nome;
@@ -38,8 +37,43 @@ export class editClientUseCase {
     const updatedCpf = newCpfValue
       ? new Cpf({ value: newCpfValue, emissionDate: newEmissionDate })
       : clientToBeEdited.getCpf;
+    let updatedCellphones = clientToBeEdited.getCellphones;
+    const cellphoneArray = new Array<Cellphone>();
+    for (let index = 0; index < updatedCellphones.length; index++) {
+      const newPhoneDDD = await this.input.textInput(
+        `Insira o novo ddd do ${index} telefone`,
+      );
+      const newPhoneNumber = await this.input.textInput(
+        `Insira o novo numer do ${index} telefone`,
+      );
+      const cellphone = new Cellphone({
+        number: newPhoneNumber,
+        ddd: newPhoneDDD,
+      });
+      cellphoneArray.push(cellphone);
+    }
+    let updatedRgs = clientToBeEdited.getRgs;
+    const RgArray = new Array<Rg>();
+    for (let index = 0; index < updatedRgs.length; index++) {
+      const newRgValue = await this.input.textInput(
+        `Insira o novo valor do ${index} rg`,
+      );
+      const dateInput = await this.input.textInput(
+        `Insira a nova data de emissao  do ${index} rg`,
+      );
+      const newEmissiondate = this.parseCpfDate(dateInput);
+      const newRG = new Rg({
+        emissionDate: newEmissiondate,
+        value: newRgValue,
+      });
+      RgArray.push(newRG);
+    }
 
+    updatedRgs = RgArray;
+    updatedCellphones = cellphoneArray;
     clientToBeEdited.nome = newName;
+    clientToBeEdited.setRgs = updatedRgs;
+    clientToBeEdited.setCellphones = updatedCellphones;
     clientToBeEdited.nomeSocial = newSocialName;
     clientToBeEdited.setCpf = updatedCpf;
     console.log("Cliente atualizado com sucesso");
